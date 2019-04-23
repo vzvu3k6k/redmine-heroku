501df0427f0c5b82311c88c6c8c960dc4798e305
vzvu3k6k <vzvu3k6k@gmail.com>
2019-04-24 00:39:52 +0900

    Print admin's password in build log

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,6 @@
 set -e
 
+echo "admin's password: $(cat "$ENV_DIR/ADMIN_PASSWORD")"
+
 curl "https://www.redmine.org/releases/redmine-$(cat "$ENV_DIR/REDMINE_VERSION").tar.gz" | tar xz --strip 1
 mv overlays/config/* config
