58e97a482b96f0d6600fc3d0d942b252ff960dc4
vzvu3k6k <vzvu3k6k@gmail.com>
2019-05-01 01:20:25 +0900

    Support user overlay

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -3,4 +3,12 @@ set -e
 echo "admin's password: $(cat "$ENV_DIR/ADMIN_PASSWORD")"
 
 curl "https://www.redmine.org/releases/redmine-$(cat "$ENV_DIR/REDMINE_VERSION").tar.gz" | tar xz --strip 1
+
 mv overlays/config/* config
+
+OVERLAY_URL=$(cat "$ENV_DIR/OVERLAY_URL")
+if [ ! -z "$OVERLAY_URL" ]; then
+  git clone --depth 1 "$OVERLAY_URL" custom_overlay
+  git -C custom_overlay submodule update --init --recursive --recommend-shallow --depth 1
+  rsync -a custom_overlay/. .
+fi
